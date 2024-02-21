fa9b56195f875bef01f31614ab41eb6bde1e59be
SULAPIS <sulapis@outlook.com>
2024-02-21 16:28:30 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import * as core from '@actions/core';
-import { context, getOctokit } from '@actions/github';
-import type { GraphQlQueryResponseData } from "@octokit/graphql";
+import * as core from '@actions/core'
+import { context, getOctokit } from '@actions/github'
+import type { GraphQlQueryResponseData } from '@octokit/graphql'
 
 export async function run(): Promise<void> {
   try {
-    const accessToken = core.getInput('access-token');
-    const close_count = parseInt(core.getInput('close_count'));
-    const prNumber = context.payload.pull_request?.number;
-    const owner = context.repo.owner;
-    const repo = context.repo.repo;
+    const accessToken = core.getInput('access-token')
+    const close_count = parseInt(core.getInput('close_count'))
+    const prNumber = context.payload.pull_request?.number
+    const owner = context.repo.owner
+    const repo = context.repo.repo
 
     if (!prNumber) {
-      core.setFailed('No PR number found');
-      return;
+      core.setFailed('No PR number found')
+      return
     }
 
-    const client = getOctokit(accessToken);
+    const client = getOctokit(accessToken)
 
     const result = await client.graphql<GraphQlQueryResponseData>(
       `{
@@ -30,19 +30,19 @@ export async function run(): Promise<void> {
         }
       }
       `
-    );
-    const closingIssues = result.repository.pullRequest.closingIssuesReferences.nodes;
+    )
+    const closingIssues =
+      result.repository.pullRequest.closingIssuesReferences.nodes
 
     for (const issue of closingIssues) {
-      const issueNumber: number = issue.number;
+      const issueNumber: number = issue.number
       await client.rest.issues.update({
         owner,
         repo,
         issue_number: issueNumber,
         state: 'closed'
-      });
+      })
     }
-
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
